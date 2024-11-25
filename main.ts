2a94303f1bbcdbe525f742bd9ce5d429a191d485
rezishon <rezashahmoradi.81@outlook.com>
2024-11-26 02:51:25 +0330

    feat: replace to new uniqe instance of the Files

--- main.ts
+++ main.ts
@@ -62,7 +62,7 @@ export default class DisciplineReporterPlugin extends Plugin {
 		this.addCommand({
 			id: "open-routine-recorder-file",
 			name: "Open routine recorder file",
-			callback: () => new Files(this.app).CreateRoutinesTrackingFile(),
+			callback: () => files.CreateRoutinesTrackingFile(),
 		});
 
 		//#endregion
