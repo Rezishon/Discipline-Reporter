46216e35853130016ad974b22ae50c1a92037458
rezishon <rezashahmoradi.81@outlook.com>
2024-12-07 00:46:16 +0330

    feat: add commands interface
    Commands list should have a interface which will let us to access each
    parameter of our data object

--- routines-commands.ts
+++ routines-commands.ts
@@ -1,2 +1,10 @@
 import DisciplineReporterPlugin from "main";
 import Files from "routines-dailyFileClass";
+
+interface _CommandsInfo {
+	[commandNames: string]: {
+		id: string;
+		name: string;
+		callback: () => void;
+	};
+}
