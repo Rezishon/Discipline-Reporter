fe7bddb1b16b7152962aab6d6d252625c409866d
rezishon <rezashahmoradi.81@outlook.com>
2025-01-19 13:56:37 +0330

    fix: add unit test (Jest) config file
    By testing multi multi multiple codes and methods, this was what runs
    unit tests without unknow errors.
    Transform make the file transform to JS files and then runs test on
    them.
    autoMock made errors so I commented that line. The mockking according to
    what i red, it's for faking different modules. Used when we have nested
    imports for testing.
    Module mapper inialize module pathes for Jest framework to be used on
    unit tests methods

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
-  transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
-  },
+	testEnvironment: "node",
+	transform: {
+		"^.+\\.(ts|tsx)$": "ts-jest",
+	},
+	// automock: true,
+	moduleNameMapper: {
+		"^obsidian$": "<rootDir>/node_modules/obsidian/obsidian.d.ts",
+		"@codemirror": "<rootDir>/node_modules/@codemirror/",
+	},
 };
